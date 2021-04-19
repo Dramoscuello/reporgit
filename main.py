e165cdc0105462641461bf97ccc2f3be89d8aa39
Deimer Ramos Cuello <dramos.cuello@gmail.com>
2021-04-19 12:11:46 -0500

    ultimo cambio 19-04

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def main():
-    pass
+def eje1():
+    tope = int(input("Dijite el limite: "))
+    suma = 0
+    for i in range(1, tope+1):
+        suma = suma + i
+    return suma/tope
+
+def eje2():
+    tope = int(input("Dijite el limite: "))
+    suma = 0
+    if tope % 2 != 0:
+        return round(tope/2) + 1
+    else:
+        for i in range(1, tope+1):
+            suma = suma + i
+        return suma/tope
+        
+
 
 if __name__ == '__main__':
-    main()
+    #print("La media es: " + str(eje1()))
+    print("La mediana es: " + str(eje2()))
+    #1,2,3,4,5,6,7,8,9,10
