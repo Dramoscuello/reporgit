05d3a0655e4513b204ac211dcf567c685b5b2e45
Deimer Ramos Cuello <dramos.cuello@gmail.com>
2021-04-19 12:46:37 -0500

    funcion anonima par agregada

--- main.py
+++ main.py
@@ -19,5 +19,8 @@ def eje2():
 
 if __name__ == '__main__':
     #print("La media es: " + str(eje1()))
-    print("La mediana es: " + str(eje2()))
+    #print("La mediana es: " + str(eje2()))
     #1,2,3,4,5,6,7,8,9,10
+
+    es_par = lambda x: x % 2 == 0
+    print(es_par(7))
